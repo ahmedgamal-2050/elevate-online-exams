5d3f3e2cc79b7c9d9ad3afc7184bed055702b92f
ahmed.gamal <ahmed.gamal@areebgroup.com>
2025-11-02 10:27:14 +0200

    feat: create auth layout - stablish routes - initialize design system

--- apps/elevate-online-exams/src/app/app.routes.ts
+++ apps/elevate-online-exams/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Route } from '@angular/router';
+import { AppRoutes } from './core/enum/app-routes';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: AppRoutes.auth.root,
+    loadComponent: () =>
+      import('./layout/auth-layout/auth-layout.component').then(
+        (m) => m.AuthLayoutComponent
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: AppRoutes.auth.root + '/' + AppRoutes.auth.login,
+  },
+  {
+    path: '',
+    redirectTo: AppRoutes.auth.root + '/' + AppRoutes.auth.login,
+    pathMatch: 'full',
+  },
+];
---
+++ apps/elevate-online-exams/src/app/core/enum/app-routes.ts
@@ -0,0 +1,6 @@
+export const AppRoutes = {
+  auth: {
+    root: 'auth',
+    login: 'login',
+  },
+} as const;
