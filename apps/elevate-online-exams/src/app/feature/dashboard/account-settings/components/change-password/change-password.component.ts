2d94985df8ea793850d4f1dd2edd423fb4ade3f4
ahmed.gamal <ahmed.gamal@areebgroup.com>
2025-12-09 12:59:47 +0200

    feat: handle api error message in change password

--- apps/elevate-online-exams/src/app/feature/dashboard/account-settings/components/change-password/change-password.component.ts
+++ apps/elevate-online-exams/src/app/feature/dashboard/account-settings/components/change-password/change-password.component.ts
@@ -1,16 +1,18 @@
 import { Component, input, output, signal } from '@angular/core';
 import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { NgTemplateOutlet } from '@angular/common';
+import { ApiErrorMessageComponent } from 'apps/elevate-online-exams/src/app/shared/components/api-error-message/api-error-message.component';
 
 @Component({
   selector: 'app-change-password',
-  imports: [ReactiveFormsModule, NgTemplateOutlet],
+  imports: [ReactiveFormsModule, NgTemplateOutlet, ApiErrorMessageComponent],
   templateUrl: './change-password.component.html',
   styleUrl: './change-password.component.css',
 })
 export class ChangePasswordComponent {
   passwordForm = input.required<FormGroup>();
-  isLoading = input<boolean>();
+  isLoading = input<boolean>(false);
+  errorMessage = input<string>('');
   changePassword = output<void>();
 
   showPassword = signal<{
